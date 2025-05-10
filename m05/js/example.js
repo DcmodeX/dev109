138dc606f7d843dc525d516e65dd747e1ccba062
Azimov Dastan <125632763+DcmodeX@users.noreply.github.com>
2025-05-09 17:19:50 -0700

    Update example.js

--- m05/js/example.js
+++ m05/js/example.js
@@ -1,1 +1,19 @@
+let userNumberInput = prompt("Please enter a number between 0 and 10:", "");
 
+let number = parseInt(userNumberInput);
+if (isNaN(number) || userNumberInput === null) {
+  number = 0;
+} else if (number < 0) {
+  number = 0; 
+} else if (number > 10) {
+  number = 10; 
+}
+
+let msg = '<h2>Multiplication Table</h2>';
+
+for (let i = 1; i <= 10; i++) {
+  msg += '<li>' + i + ' x ' + number + ' = ' + (number * i) + '</li>';
+}
+
+const multiplicationTableDiv = document.getElementById('multiplicationTable');
+multiplicationTableDiv.innerHTML = msg;
